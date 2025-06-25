5b2e819650b631167887f03d8c21cd80b480591e
Sara Doytch <sara0527656182@gmail.com>
2025-06-25 22:34:41 +0300

    setup-store-router

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,14 +1,27 @@
 
-import { Outlet } from 'react-router';
-
+// import { Outlet } from 'react-router';
+import { Outlet, Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Container, Button } from "@mui/material";
 const AppLayout = () => {
 
   return (
-     <div style={{ padding: "2rem" }}>
-      <h1>📦 רשימת מוצרים</h1>
-      <Outlet />
-    </div>
+    //  <div style={{ padding: "2rem" }}>
+    //   <h1>📦 רשימת מוצרים</h1>
+    //   <Outlet />
+    // </div>
+    <>
+     <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>🔷 מערכת מוצרים</Typography>
+          <Button color="inherit" component={Link} to="/">בית</Button>
+        </Toolbar>
+      </AppBar>
+      <Container sx={{ mt: 4 }}>
+        <Outlet />
+      </Container>
+      </>
   );
 };
 
 export default AppLayout;
+
